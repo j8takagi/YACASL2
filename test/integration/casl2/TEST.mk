f9e27534c723ae199ed32404047b9a2c6519a8a9
j8takagi <j8takagi@nifty.com>
2010-03-25 23:54:24 +0900

    テストを、MacやCygwinでも成功するように修正

--- test/integration/casl2/TEST.mk
+++ test/integration/casl2/TEST.mk
@@ -4,7 +4,6 @@
 # make prepare : CMDで設定されたコマンドを実行した出力結果を0.txt（テストの想定結果）に出力
 # make clean   : 「make」で生成されたファイルをクリア
 # make cleanall: 「make」と「make clean」で生成されたファイルをクリア
-ERRFILE = err.txt
 UNITNAME = `pwd | xargs basename`
 
 .PHPNY: check prepare clean cleanall
@@ -14,10 +13,12 @@ clean:
 	@rm -f 1.txt diff.txt report.txt err.txt
 cleanall: clean
 	@rm -f 0.txt
-0.txt 1.txt:: $(CASL2) $(ASFILE)
+ifndef SETBYOTHER
+0.txt 1.txt: $(CASL2) $(ASFILE)
 	@echo $(CMD) >$@; \
-     $(CMD) >>$@ 2>$(ERRFILE); \
-     if test -s $(ERRFILE); then cat err.txt >>$@; else rm -f $(ERRFILE); fi
+     $(CMD) >>$@ 2>err.txt; \
+     if test -s err.txt; then cat err.txt >>$@; else rm -f err.txt; fi
+endif
 diff.txt: 1.txt
 	@-diff -c 0.txt 1.txt >$@ 2>&1
 report.txt: diff.txt
