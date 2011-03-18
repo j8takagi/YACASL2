641024682d02a49ea9fed303c2f6db4b4bd1d9c1
j8takagi <j8takagi@nifty.com>
2011-03-19 00:24:30 +0900

    comet2コマンドのシステムテストで、オブジェクトファイルを直接実行するテストを追加
    directを追加し、err_209とerr_210を変更

---
+++ test/system/comet2/err_209/create_a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "cmem.h"
+#include "word.h"
+
+/**
+ * 引数で指定したファイルにアセンブル結果を書込
+ */
+int main()
+{
+    FILE *fp;
+    const WORD w[] = {0x1010, 0x0005, 0x220f, 0x0006, 0x8100, 0x0003, 0x0001};
+    const char *f = "a.comet";
+
+    if((fp = fopen(f, "w")) == NULL) {
+        perror(f);
+        exit(-1);
+    }
+    fwrite(w, sizeof(WORD), ARRAYSIZE(w), fp);
+    fclose(fp);
+    return 0;
+}
