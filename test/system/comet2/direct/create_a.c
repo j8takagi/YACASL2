4027d87a62ceedf7c387cc078506f14ac9d958e3
j8takagi <j8takagi@nifty.com>
2011-04-04 23:03:39 +0900

    テストの整理

--- test/system/comet2/direct/create_a.c
+++ test/system/comet2/direct/create_a.c
@@ -10,7 +10,7 @@ int main()
 {
     FILE *fp;
     const WORD w[] = {0x1010, 0x0005, 0x2210, 0x0006, 0x8100, 0x0003, 0x0001};
-    const char *f = "a.comet";
+    const char *f = "a.o";
 
     if((fp = fopen(f, "w")) == NULL) {
         perror(f);
